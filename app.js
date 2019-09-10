fe40cf68368aceeff87e1e052408d9dc6e15d2e1
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-10 13:52:47 -0700

    cambiosb

--- app.js
+++ app.js
@@ -24,5 +24,22 @@ const keyNoteDown = event =>
     const button= document.querySelector(`button[data-key="${key}"]`)
     if (button) button.click();
 } 
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
 
 document.addEventListener('keydown', keyNoteDown);
